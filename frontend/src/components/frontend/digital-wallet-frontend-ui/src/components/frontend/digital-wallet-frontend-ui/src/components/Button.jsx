b2d2fa4b3a890abbff97e1f69f4b9625ac1caea5
chandrasekhar <majjarichandrasekhar04@gmail.com>
2024-12-27 14:39:38 +0530

    Create Button.jsx

---
+++ frontend/src/components/frontend/digital-wallet-frontend-ui/src/components/frontend/digital-wallet-frontend-ui/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button() {
+  return (
+    <>
+      <input
+        type="submit"
+        value="Signup"
+        className="bg-black text-white font-semibold text-xl rounded-md w-1/2 m-auto p-1 mt-4 cursor-pointer"
+      />
+    </>
+  );
+}
+
+export default Button;
